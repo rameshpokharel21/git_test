442d21bbcb9df8c5f7700e0acd5a0d630aec6ca1
Ramesh Pokharel <pokharelramesh@yahoo.com>
2024-01-20 20:14:43 -0500

    formatting aligned

--- Hello.java
+++ Hello.java
@@ -11,12 +11,12 @@ public class Hello{
         int i = 0;
 
         for(var x : twod){
-        i = 0;//reset i here
+            i = 0;//reset i here
             while(i < x.length){//x is nested array
                 System.out.print(x[i] + "\t");
                 ++i;
             }
-        System.out.println();//new line after each row
+            System.out.println();//new line after each row
 
         }//closing for loop
     }
