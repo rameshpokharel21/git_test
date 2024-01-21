4232a0970a086b51c73a8beb3c0c012227cfb81f
Ramesh Pokharel <pokharelramesh@yahoo.com>
2024-01-20 19:42:17 -0500

    removed hello.txt and added Hello.java

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*hello how are you?
+I am fine. Thank you!
+mixing for-each loop and for loop in 2d array
+*/
+public class Hello{
+    public static void main(String[] args) {
+        
+        int[][] twod = {{5, 9, 2}, {7, 1, 3}, {9, 0}};
+
+        int i = 0;
+
+        for(var x : twod){
+        i = 0;//reset i here
+        while(i < x.length){//x is nested array
+            System.out.print(x[i] + "\t");
+            ++i;
+        }
+        System.out.println();//new line after each row
+        }//closing for loop
+    }
+}
+
